54fcd54c0216170797d02de056d87f7af9ccc3c2
Simran Gandhi <gandhi.k.simran@gmail.com>
2024-11-04 21:21:35 -0800

    adding main and logout page

--- src/screens/Logout.js
+++ src/screens/Logout.js
@@ -1,9 +1,84 @@
 import { Link} from "react-router-dom"
+import Navbar from "../components/navbar"
+import userImage from "../assests/images/logo.png"
 
 export function Logout() {
     return (
-        <> 
-        <h1>This is the logout page!</h1>
+        <>
+          <Navbar />
+          <div
+            style={{
+              backgroundColor: '#D6E2E8',
+              minHeight: '100vh',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              padding: '20px',
+            }}
+          >
+            {/* Container for Logo and Sign-Up Box */}
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: '60px',
+              }}
+            >
+              {/* Logo Image */}
+              <img
+                src={userImage}
+                alt="User Logo"
+                style={{ width: '400px', 
+                  height: '400px', 
+                  position: 'relative', 
+                  bottom: '100px'}}
+              />
+    
+              {/* Sign-Up Box */}
+              <div
+                style={{
+                  backgroundColor: '#91C1D8',
+                  width: '300px',
+                  height: '330px',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center', 
+                  position: 'relative', 
+                  bottom: '100px'
+                }}
+              >
+                <div
+                  style={{
+                    color: 'white',
+                    fontFamily: 'Avenir',
+                    fontSize: '40px',
+                    textAlign: 'center',
+                    marginBottom: '10px',
+                  }}
+                >
+                  See you Soon!
+                </div>
+                <button
+                  style={{
+                    backgroundColor: '#003371',
+                    width: '150px',
+                    padding: '10px',
+                    borderRadius: '30px',
+                    color: 'white',
+                    fontFamily: 'Avenir',
+                    fontSize: '16px',
+                    border: 'none',
+                    cursor: 'pointer',
+                    position: 'relative', 
+                    top: '40px'
+                  }}
+                >
+                  Sign In
+                </button>
+              </div>
+            </div>
+          </div>
         </>
-    )
+      );
 }
