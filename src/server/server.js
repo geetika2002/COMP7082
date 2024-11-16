584642e58b6084d5af781fcef0f15f2ac3da0728
s-Kone <Connie-jay@hotmail.com>
2024-11-15 17:49:58 -0800

    db setup

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 // import cors from 'cors';
-// import users from "./routes/user.js";
+import users from "./routes/user.js";
+import db from './connection.js';
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -8,7 +9,12 @@ const app = express();
 // app.use(cors());
 app.use(express.json());
 
-// app.use("/user", users);
+app.use("/user", users);
+
+app.use((req, res, next) => {
+    req.db = db; // Attach db to the request object
+    next();
+  });
 
 // start the Express server
 app.listen(PORT, () => {
