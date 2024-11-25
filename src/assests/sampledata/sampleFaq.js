dcf09934c7983403dc32bab7378e251e90fc4a7d
s-Kone <Connie-jay@hotmail.com>
2024-11-25 09:35:46 -0800

    faq page

---
+++ src/assests/sampledata/sampleFaq.js
@@ -0,0 +1,39 @@
+const faqData = [
+    {
+      id: 1,
+      question: "Is the site free to use?",
+        answer: "Yes, our site is free to use with access to all interview questions and resources curated by our team."
+    },
+    {
+        id: 2,
+        question: "Who is this site made for?",
+        answer: "Our site can be used by anyone looking to break into tech and learn different aspects of a tech interview"
+      },
+      {
+        id: 3,
+        question: "Can I receive feedback on my answers?",
+        answer: "At this moment, our platform offers the optimal answer, but we're working towards adding personalized feedback."
+      },
+      {
+        id: 4,
+        question: "How often are new questions added?",
+        answer: "We regularly update our content to match the fast changing industry."
+      },
+      {
+        id: 5,
+        question: "How do I use the interview questions?",
+      answer: "Choose a question you want to practice, and you can view suggested answer, tips for responding, and additional resources for improving your skills."
+      },
+      {
+        id: 6,
+        question: "How can I track my progress?",
+        answer: "We're working on a progress feature that will allow you to track questions you've answered and which ones you'd like to revisit."
+      },
+      {
+        id: 7,
+        question: "Can I record my interview sessions?",
+        answer: "Currently we do not offer recorded interview sessions, we are working on the AI mock interview feature for future relaese."
+      },
+    ];
+
+    export default faqData;
