4a992a7672f3751ee1c798b19cd9c764cdbd18e4
s-Kone <Connie-jay@hotmail.com>
2024-11-25 08:10:57 -0800

    about page

--- src/screens/about.js
+++ src/screens/about.js
@@ -1,14 +1,26 @@
 import { Link} from "react-router-dom"
 import Navbar from "../components/navbar"
-import userImage from "../assests/images/logo.png"
+// import userImage from "../assests/images/logo.png"
+import "../styles/about.css"
+import teampic from "../assests/images/team-awesome-minions.jpg"
+
 
 export function About() {
     return (
         <>
         <Navbar></Navbar>
-        <h1>This is the About us page</h1>
-        
-        <Link to="/">Homepage</Link>
+          <div className="about-container">
+            <header className="about-header">
+              <p className="about-subtitle">Our vision is to have a platform for jobseeker's to build confidence in their interviewing skills and secure the job they want.</p>
+              <h1 className="about-title">About Team Awesome</h1>
+            </header>
+            <div className="about-content">
+              <p className="about-team-description">
+                We are first term BASc students from the Network Security and Game Development options.
+              </p>
+              <img src={ teampic } alt="team picture"className="team-image"></img>
+          </div>
+      </div>
         </>
     )
 }
